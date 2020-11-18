71e3fc887451bcd78ee8a838a6a359c2df136cd0
Dennis Wolfers <dennis.wolfers@gmail.com>
2020-11-18 13:45:05 -0800

    phonebook backend 3.2 complete

--- index.js
+++ index.js
@@ -32,6 +32,16 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/api/info", (request, response) => {
+  response.send(
+    "<p>Phonebook has info for " +
+      persons.length +
+      " people.</p><p>" +
+      new Date().toString() +
+      "</p>"
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
