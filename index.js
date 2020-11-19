fadc79184e29431d5b9a03a159b27af498cb40e3
Dennis Wolfers <dennis.wolfers@gmail.com>
2020-11-19 13:27:06 -0800

    phonebook backend 3.5 partial

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     name: "Arto Hellas",
@@ -58,6 +60,13 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const person = request.body;
+  console.log(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
