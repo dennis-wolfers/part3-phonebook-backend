6c0a543c965fdbfbda4c3f46bd721d6a0fd2213d
Dennis Wolfers <dennis.wolfers@gmail.com>
2020-12-10 07:12:52 -0800

    phonebook frontend deployment

---
+++ build/precache-manifest.e31a4cbe7f8103cfaeb9fde77cc63be5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce3af0ee5f184d4ed6e3597e2b172e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15cb2f83e40e6f180f98",
+    "url": "/static/css/main.fee6258b.chunk.css"
+  },
+  {
+    "revision": "2469ec848eef6bb55628",
+    "url": "/static/js/2.90705a08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90705a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15cb2f83e40e6f180f98",
+    "url": "/static/js/main.8bbef7a2.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
