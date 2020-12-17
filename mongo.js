9bdbd2e60f83f1b73be43814f01fcf22a79713bd
Dennis Wolfers <dennis.wolfers@gmail.com>
2020-12-17 10:07:17 -0800

    phonebook DB 3.12 implemented

--- mongo.js
+++ mongo.js
@@ -8,15 +8,12 @@ if (process.argv.length < 3) {
 }
 
 const password = process.argv[2];
-const newName = process.argv[3];
-const newNumber = process.argv[4];
-
 const url = `mongodb+srv://dFullstack:${password}@cluster0.gzblo.mongodb.net/person-app?retryWrites=true&w=majority`;
 
 mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
+  useFindAndModify: true,
   useCreateIndex: true,
 });
 
@@ -27,12 +24,25 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model("Person", personSchema);
 
-const person = new Person({
-  name: newName,
-  number: newNumber,
-});
+if (process.argv.length === 3) {
+  let list = "phonebook:";
+  Person.find({}).then((persons) => {
+    persons.map((person) => {
+      list += "\n" + person.name + " " + person.number;
+    });
+    console.log(list);
+    mongoose.connection.close();
+  });
+} else {
+  const newName = process.argv[3];
+  const newNumber = process.argv[4];
+  const person = new Person({
+    name: newName,
+    number: newNumber,
+  });
 
-person.save().then((result) => {
-  console.log(``);
-  mongoose.connection.close();
-});
+  person.save().then((result) => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
