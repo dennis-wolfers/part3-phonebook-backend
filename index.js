9c7ece28d75819434e11d702513fd346cd21ec63
Dennis Wolfers <dennis.wolfers@gmail.com>
2020-12-18 12:58:35 -0800

    phonebook backend 3.14 fixed post to check for body.name

--- index.js
+++ index.js
@@ -60,9 +60,9 @@ app.delete("/api/persons/:id", (request, response) => {
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
-  if (body.content === undefined) {
+  if (body.name === undefined) {
     return response.status(400).json({
-      error: "content missing",
+      error: "name missing",
     });
   }
 
